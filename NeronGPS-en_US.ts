c4be605f9433ce01a39383bd72e101ad12efac4c
Yann Dirson <ydirson@free.fr>
2012-03-04 20:00:02 +0100

    Sync translation files with source, including previously missing files.

---
+++ NeronGPS-en_US.ts
@@ -0,0 +1,348 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CacheForm</name>
+    <message>
+        <location filename="ui/cacheform.ui" line="23"/>
+        <source>No cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/cacheform.ui" line="36"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/cacheform.ui" line="43"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/cacheform.ui" line="50"/>
+        <source>Set-up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/cacheform.ui" line="57"/>
+        <source>Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/cacheform.ui" line="87"/>
+        <source>20</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/cacheform.ui" line="97"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/cacheform.ui" line="107"/>
+        <source>Force reload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/cacheform.ui" line="124"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ClockForm</name>
+    <message>
+        <location filename="ui/clockform.ui" line="44"/>
+        <source>GPS clock, UTC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/clockform.ui" line="57"/>
+        <source>System clock, local time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/clockform.ui" line="70"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GpxForm</name>
+    <message>
+        <location filename="ui/gpxform.ui" line="22"/>
+        <source>Records</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/gpxform.ui" line="84"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/gpxform.ui" line="91"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/gpxform.ui" line="105"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JourneyForm</name>
+    <message>
+        <location filename="ui/journeyform.ui" line="18"/>
+        <source>Location:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="32"/>
+        <source>Samples:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="49"/>
+        <source>Duration:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="74"/>
+        <source>Distance:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="99"/>
+        <source>Speed:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="124"/>
+        <source>Altitude:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="149"/>
+        <source>% fix:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="174"/>
+        <source>New trk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="181"/>
+        <source>End trk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="188"/>
+        <source>Way pt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/journeyform.ui" line="195"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardForm</name>
+    <message>
+        <location filename="ui/keyboardform.ui" line="38"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LogForm</name>
+    <message>
+        <location filename="ui/logform.ui" line="31"/>
+        <source>clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/logform.ui" line="38"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MagnificationForm</name>
+    <message>
+        <location filename="ui/magnificateform.ui" line="18"/>
+        <source>x1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/magnificateform.ui" line="25"/>
+        <source>x2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/magnificateform.ui" line="32"/>
+        <source>x4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/magnificateform.ui" line="39"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PoiForm</name>
+    <message>
+        <location filename="ui/poiform.ui" line="31"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/poiform.ui" line="38"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/poiform.ui" line="45"/>
+        <source>Go to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/poiform.ui" line="52"/>
+        <source>Drive to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/poiform.ui" line="59"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ServerForm</name>
+    <message>
+        <location filename="ui/serverform.ui" line="28"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TQtMoko</name>
+    <message>
+        <location filename="qtmoko/qtmoko.cpp" line="61"/>
+        <source>NeronGPS</source>
+        <comment>application header</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ZoomForm</name>
+    <message>
+        <location filename="ui/zoomform.ui" line="18"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="25"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="32"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="39"/>
+        <source>4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="46"/>
+        <source>5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="53"/>
+        <source>6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="60"/>
+        <source>7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="67"/>
+        <source>8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="74"/>
+        <source>9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="81"/>
+        <source>10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="88"/>
+        <source>11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="95"/>
+        <source>12</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="102"/>
+        <source>13</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="109"/>
+        <source>14</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="116"/>
+        <source>15</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="123"/>
+        <source>16</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="130"/>
+        <source>17</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="137"/>
+        <source>18</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="144"/>
+        <source>19</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="151"/>
+        <source>20</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/zoomform.ui" line="158"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
